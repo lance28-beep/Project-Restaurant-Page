514b4557e80805829172881de92ecc661fa3ddf5
Lance Valle <vallelance8@gmail.com>
2022-11-07 19:38:27 +0800

    configure webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// function component() {
+//   const element = document.createElement('div')
+
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.classList.add('hello')
+//   element.textContent = 'hello world'
+
+//   return element
+// }
+
+// document.body.appendChild(component())
+
+alert('hello')
